b098ea13d314a0b6a0f841613594d015e38defac
ZerXGIT <blackminer1242@gamil.com>
2022-05-07 15:20:44 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <limits>
+#include <vector>
+#include <sstream>
+#include <numbers>
+#include <ctime>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+
+    return 0;
+}
