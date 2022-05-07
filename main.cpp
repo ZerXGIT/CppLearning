a7b62f76a940198d22e99aaf27ba7d59d4dab292
ZerXGIT <blackminer1242@gamil.com>
2022-05-07 15:55:39 +0200

    Things

--- main.cpp
+++ main.cpp
@@ -10,7 +10,49 @@
 
 using namespace std;
 
+// Global variable
+int globalVar = 187;
+
+void printPI();
+
+// A variable that can not be changed on Runtime
+const double PI = 3.14159265359;
+
+void printPi() {
+    //Prints a formatted String
+    printf("Formatted PI: %.4f", PI);
+}
+
 int main(int argc, char** argv) {
+
+    // Prints "Hello World!" to console and ends line
+    cout << "Hello World! " << endl;
+
+    string q1 = "Enter a Number :  ";
+    string num1;
+
+
+    //Prints string to console
+    cout << q1;
+
+    // User Input
+    cin >> num1;
+
+    //Convert the string to a Number
+    int cNum1 = stoi(num1);
+
+    // Prints the Number the user has entered
+    cout << "\nYou entered the Number: " << cNum1 << endl;
+
+
+    // Create a 2D Array
+    int board[3][3] = {3,3,3,
+                       3,3,3,
+                       3,3,3};
+
+
+    //Prints Pi
+    printPi();
 
 
     return 0;
